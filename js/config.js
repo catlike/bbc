27cfbd3c3034599f2310aac44e1c6b6c1c2fe8d7
Robin Rodriguez <robinrz@amazon.com>
2023-09-16 13:34:23 -0400

    correct cognito config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_tE8nc5a8U', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'i4q5h19kk69ga4qrr0vb7rko9eh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4q5h19kk69ga4qrr0vb7rko9eh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
