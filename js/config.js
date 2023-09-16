8cf962c0a5903088197751e0010a29a7879825d6
Robin Rodriguez <robinrz@amazon.com>
2023-09-16 13:30:57 -0400

    update cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_tE8nc5a8U', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'i4q5h19kk69ga4qrr0vb7rko9eh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
